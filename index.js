b45346b354e9af1d52a590394617a1208b1b6fb4
Danill13and <dandros01@gmail.com>
2023-09-21 17:31:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+
+
+function perimeter(){
+    rl.question('Введiть перше число: ', (num1) => {
+        rl.question('Введіть друге число: ', (num2)=> {
+            num1 = parseFloat(num1)
+            num2 = parseFloat(num2) 
+
+            let result = (num1 + num2) * 2
+
+            console.log(result)
+
+        })
+    })
+}
+
+function quadratic(){
+    rl.question('Введiть перше число: ', (num1) => {
+        rl.question('Введіть друге число: ', (num2)=> {
+            rl.question('Введіть третье число', (num3)=>{
+                num1 = parseFloat(num1)
+                num2 = parseFloat(num2)
+                num3 = parseFloat(num3)
+                console.log(Math.sign(num1))
+                if(num1 != 0){
+                    if(num1 == -num1){
+                        let num1 = -1*num1
+                        let num2 = -1*num2
+                        let num3 = -1*num3
+                        let result = Math.sqrt(num2**2 -4*num1*num3)
+                        let x1 = (-num2+result)/(2*num1)
+                        let x2 = (-num2-result)/(2*num1)
+
+                        console.log(`корінь дискрімінанту${result}`)
+                        console.log(`x1 = ${x1}, x2 = ${x2}`)
+                    }else{
+                        let result = Math.sqrt(num2**2 -4*num1*num3)
+                        let x1 = (-num2 + result)/(2*num1)
+                        let x2 = (-num2 - result)/(2*num1)
+
+                        console.log(`корінь дискрімінанту${result}`)
+                        console.log(`x1 = ${x1}, x2 = ${x2}`)
+                    }
+                }else{
+                    console.log('ERROR')
+                }
+            })
+        })
+    })
+}
+quadratic()
+//perimeter()
